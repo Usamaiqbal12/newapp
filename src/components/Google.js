4aa47f124d0571238831a95cd12a0e4d315db74d
afnan-1 <roman.nadeem@gmail.com>
2021-01-23 15:22:54 +0500

    google

--- src/components/Google.js
+++ src/components/Google.js
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import GoogleLogin from "react-google-login";
-
-class Google extends Component {
-  render() {
-    const responseGoogle = (response) => {
-      console.log(response);
-    };
-    let googleData = (
-      <GoogleLogin
-        clientId="918683245824-3i4siophfobp31a542q0vumpe2l5lnhj.apps.googleusercontent.com"
-        buttonText="LOGIN WITH GOOGLE"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        scope={'profile','email'}
-        cookiePolicy={"single_host_origin"}
-      />
-    );
-    return <div>{googleData}</div>;
-  }
+function Google() {
+  const responseGoogle = (e) => {
+    console.log('hello');
+  };
+  return (
+    <GoogleLogin
+      clientId="918683245824-3i4siophfobp31a542q0vumpe2l5lnhj.apps.googleusercontent.com"
+      buttonText="LOGIN WITH GOOGLE"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      scope={("profile", "email")}
+      cookiePolicy={"single_host_origin"}
+    />
+  );
 }
 
 export default Google;
